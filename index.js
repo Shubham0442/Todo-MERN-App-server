98a00bc7f35c2b2d74f727b0f59588a4dc066693
Shubham0442 <shubhamrakhonde507@gmail.com>
2022-11-05 19:20:01 +0530

    cors middleware added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const { connection } = require("./Config/db");
 const { userRouter } = require("./Routes/user.route");
 const { TodoRouter } = require("./Routes/todo.route")
+const cors = require('cors')
 
 require("dotenv").config()
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 5241
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res)=>{
     res.send("Welcome to home")
